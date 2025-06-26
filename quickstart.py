88d1d0b3dd735cf9564d3fe9d5f72a069d10cbc2
Jifeng Wu <jifengwu2k@gmail.com>
2025-06-25 21:39:28 -0400

    2025年 06月 25日 星期三 21:39:28 EDT

--- quickstart.py
+++ quickstart.py
@@ -15,7 +15,7 @@ model = ESM3_sm_open_v0(
     keyword_vocabulary_path='models--EvolutionaryScale--esm3-sm-open-v1/snapshots/2feda652c51c57f9797fc1b01f8d6181b38fb488/data/keyword_vocabulary_safety_filtered_58641.txt',
     lsh_hyperplanes_path='models--EvolutionaryScale--esm3-sm-open-v1/snapshots/2feda652c51c57f9797fc1b01f8d6181b38fb488/data/hyperplanes_8bit_58641.npz',
     residue_annotations_csv_path='models--EvolutionaryScale--esm3-sm-open-v1/snapshots/2feda652c51c57f9797fc1b01f8d6181b38fb488/data/uniref90_and_mgnify90_residue_annotations_gt_1k_proteins.csv',
-    esm3_sm_open_v1_pth_path='models--EvolutionaryScale--esm3-sm-open-v1/snapshots/2feda652c51c57f9797fc1b01f8d6181b38fb488/data/weights/esm3_sm_open_v1.pth.pth',
+    esm3_sm_open_v1_pth_path='models--EvolutionaryScale--esm3-sm-open-v1/snapshots/2feda652c51c57f9797fc1b01f8d6181b38fb488/data/weights/esm3_sm_open_v1.pth',
 )
 
 if device.type != "cpu":
